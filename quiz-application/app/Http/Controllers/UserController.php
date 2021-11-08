6b25b65678c049ca53fe70f071b8468c10ed4fd9
shreyosi-roy <shreyosi@brandconfiance.com>
2021-11-08 19:50:21 +0530

    Model Created

--- quiz-application/app/Http/Controllers/UserController.php
+++ quiz-application/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\UserModel;
 
 
 class UserController extends Controller
